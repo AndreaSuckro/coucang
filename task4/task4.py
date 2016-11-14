db03e9525a32e8c318dc8b9406e7e34b5da55f8f
Andrea <a.suckro@gmail.com>
2016-11-14 16:18:26 +0100

    initial commit with data ignore added for 4

---
+++ task4/task4.py
@@ -0,0 +1,12 @@
+"""
+Solution of the Group Sunda Slow Loridae
+
+Task 4 - Convolutional Neural Networks
+"""
+import tensorflow as tf
+import numpy as np
+from mnist import MNIST
+
+data = MNIST()
+
+# add magic single dimension to data
