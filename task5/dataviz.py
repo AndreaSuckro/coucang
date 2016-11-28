0cb65945cde5e62c7ce41bd2022208e33cb4c761
Sebastian Höffner <info@sebastian-hoeffner.de>
2016-11-28 17:32:31 +0100

    dataviz as functions to use with ipynb

--- task5/dataviz.py
+++ task5/dataviz.py
@@ -1,13 +1,21 @@
 from matplotlib import pyplot as plt
-from data import Data
-from numpy import squeeze
+import numpy as np
 
-data = Data()
 
-plt.suptitle('Data Samples')
-for i, (img, label) in enumerate(zip(*data.get_batch(5*12, data.test))):
-    ax = plt.subplot(5, 12, 1+i)
-    ax.set_title(label)
-    ax.axis('off')
-    ax.imshow(squeeze(img), cmap='gray')
-plt.show()
+def plot_samples(data, size=(5, 12)):
+    """Plots some random samples.
+
+    Args:
+        data: a Data object
+        size: the number of samples to plot (rows, columnss)
+    """
+    plt.figure()
+    plt.suptitle('Random Data Samples')
+    for i, (img, label) in enumerate(zip(*data.get_batch(np.multiply(*size), data.test))):
+        ax = plt.subplot(size[0], size[1], 1+i)
+        ax.set_title(label)
+        ax.axis('off')
+        ax.imshow(np.squeeze(img), cmap='gray')
+    plt.show()
+
+
